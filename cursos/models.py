7b5ec1a951df9e08b8ec729d3a89bc469ebaeb52
Maíra Ramos <ramosmaira369@gmail.com>
2025-11-20 10:05:58 -0300

    Criar model Curso e adicionar migrations

--- cursos/models.py
+++ cursos/models.py
@@ -1,1 +1,9 @@
-# Arquivo models.py do app cursos
+from django.db import models
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    carga_horaria = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nome
