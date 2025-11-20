9f450d654d4a8b229c97d14d810f70772616588c
Maíra Ramos <ramosmaira369@gmail.com>
2025-11-20 15:20:42 -0300

    Configurar URLs do CRUD de cursos

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.listar_cursos, name='listar_cursos'),
+    path('criar/', views.criar_curso, name='criar_curso'),
+    path('<int:id>/', views.detalhe_curso, name='detalhe_curso'),
+    path('<int:id>/editar/', views.editar_curso, name='editar_curso'),
+    path('<int:id>/excluir/', views.excluir_curso, name='excluir_curso'),
+]
