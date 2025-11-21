e3b192715021a1f9c41806233121c4bc5f8b1985
Maíra Ramos <ramosmaira369@gmail.com>
2025-11-20 22:11:09 -0300

    Implementando views do CRUD de cursos

--- cursos/views.py
+++ cursos/views.py
@@ -1,1 +1,39 @@
-# Arquivo views.py do app cursos
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Curso
+from .forms import CursoForm
+
+def listar_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'cursos/lista.html', {'cursos': cursos})
+
+def detalhe_curso(request, id):
+    curso = get_object_or_404(Curso, id=id)
+    return render(request, 'cursos/detalhe.html', {'curso': curso})
+
+def criar_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_cursos')
+    else:
+        form = CursoForm()
+    return render(request, 'cursos/form.html', {'form': form, 'titulo': 'Criar Curso'})
+
+def editar_curso(request, id):
+    curso = get_object_or_404(Curso, id=id)
+    if request.method == 'POST':
+        form = CursoForm(request.POST, instance=curso)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_cursos')
+    else:
+        form = CursoForm(instance=curso)
+    return render(request, 'cursos/form.html', {'form': form, 'titulo': 'Editar Curso'})
+
+def excluir_curso(request, id):
+    curso = get_object_or_404(Curso, id=id)
+    if request.method == 'POST':
+        curso.delete()
+        return redirect('listar_cursos')
+    return render(request, 'cursos/confirmar_excluir.html', {'curso': curso})
