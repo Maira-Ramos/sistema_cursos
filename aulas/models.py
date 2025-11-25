dbca98372161d4df38383c4d1f4e1b926f45c54b
samaramerces1 <samaramerces746@gmail.com>
2025-11-25 11:29:38 -0300

    Atualização no models do app aulas

--- aulas/models.py
+++ aulas/models.py
@@ -1,3 +1,12 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from cursos.models import Modulo
+
+class Aula(models.Model):
+    modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE, related_name='aulas')
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    link_video = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
