cc7515a009e5610bbfd2168c2f327067da91c245
Maíra Ramos <ramosmaira369@gmail.com>
2025-11-26 12:10:38 -0300

    Alterando modelo de usuários

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,1 +1,11 @@
-# Arquivo models.py do app usuarios
+from django.db import models
+from django.contrib.auth.models import User
+
+class PermissaoCustom(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pode_criar_curso = models.BooleanField(default=False)
+    pode_editar_aluno = models.BooleanField(default=False)
+    pode_deletar_postagem = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Permissões de {self.user.username}'
