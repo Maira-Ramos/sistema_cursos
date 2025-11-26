831b8f63237ea08ae3ed5d0396809d9030ab545d
Maíra Ramos <ramosmaira369@gmail.com>
2025-11-26 12:11:29 -0300

    Atualizando as rotas de usuários

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.lista_usuarios, name='lista_usuarios'),
+    path('novo/', views.criar_usuario, name='criar_usuario'),
+    path('editar/<int:user_id>/', views.editar_usuario, name='editar_usuario'),
+    path('deletar/<int:user_id>/', views.deletar_usuario, name='deletar_usuario'),
+]
