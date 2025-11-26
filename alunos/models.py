8b61b82d7ad1227c810db1cb366c0a775b1877ac
samaramerces1 <samaramerces746@gmail.com>
2025-11-26 00:31:38 -0300

    Criar Model alunos e adicionar migrations

--- alunos/models.py
+++ alunos/models.py
@@ -1,1 +1,13 @@
-# Arquivo models.py do app alunos
+from django.db import models
+from cursos.models import Curso  # Ajuste se o nome da app de cursos for outro
+
+class Aluno(models.Model):
+    nome = models.CharField("Nome completo", max_length=200)
+    email = models.EmailField("E-mail", unique=True)
+    data_nascimento = models.DateField("Data de nascimento")
+
+    def __str__(self):
+        return self.nome
+
+
+
