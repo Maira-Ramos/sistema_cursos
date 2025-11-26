63f37502657d198b9cd95367f7c0cf2560944719
Maíra Ramos <ramosmaira369@gmail.com>
2025-11-26 01:05:23 -0300

    Ajustando rotas dos módulos

--- modulos/urls.py
+++ modulos/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.listar_modulos, name="listar_modulos"),
+    path("<int:id>/", views.detalhe_modulo, name="detalhe_modulo"), 
     path("criar/", views.criar_modulo, name="criar_modulo"),
     path("editar/<int:id>/", views.editar_modulo, name="editar_modulo"),
     path("excluir/<int:id>/", views.excluir_modulo, name="excluir_modulo"),
