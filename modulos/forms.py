6f1063b33deb6f29914d35fe86ceffbd95db0ed7
Maíra Ramos <ramosmaira369@gmail.com>
2025-11-26 12:12:57 -0300

    Ajustando forms do módulo

--- modulos/forms.py
+++ modulos/forms.py
@@ -1,14 +1,14 @@
+# modulos/forms.py
+
 from django import forms
 from .models import Modulo
-from cursos.models import Curso
 
 class ModuloForm(forms.ModelForm):
     class Meta:
         model = Modulo
-        fields = ["curso", "nome", "descricao"]
-
+        fields = ['nome', 'descricao', 'curso']
         widgets = {
-            "curso": forms.Select(attrs={"class": "form-select"}),
-            "nome": forms.TextInput(attrs={"class": "form-control"}),
-            "descricao": forms.Textarea(attrs={"class": "form-control", "rows": 4}),
+            'nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome do módulo'}),
+            'descricao': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descrição do módulo'}),
+            'curso': forms.Select(attrs={'class': 'form-select'}),
         }
