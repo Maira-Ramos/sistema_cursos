c2018e912c9c761c388b484ebb747d62a2a1a2d5
samaramerces1 <samaramerces746@gmail.com>
2025-11-26 21:52:06 -0300

    Correção da views e urls do app inscrições

--- inscricoes/urls.py
+++ inscricoes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'inscricoes'
+app_name = "inscricoes"
 
 urlpatterns = [
-    path('criar/', views.criar_inscricao, name='criar'),
+    path("", views.listar_inscricoes, name="listar"),
+    path("criar/", views.criar_inscricao, name="criar"),
+    path("<int:id>/", views.detalhes_inscricao, name="detalhes"),
+    path("<int:id>/excluir/", views.excluir_inscricao, name="excluir"),
 ]
--- inscricoes/views.py
+++ inscricoes/views.py
@@ -3,24 +3,51 @@ from django.contrib import messages
 from .models import Inscricao
 from .forms import InscricaoForm
 
+def listar_inscricoes(request):
+    inscricoes = Inscricao.objects.all()
+    return render(request, "inscricoes/lista.html", {"inscricoes": inscricoes})
+
+
+# DETALHES
+def detalhes_inscricao(request, id):
+    inscricao = get_object_or_404(Inscricao, id=id)
+    total = Inscricao.objects.filter(aluno=inscricao.aluno).count()
+
+    return render(request, "inscricoes/detalhes.html", {
+        "inscricao": inscricao,
+        "total": total
+    })
+
+
+# CRIAR
 def criar_inscricao(request):
     if request.method == "POST":
         form = InscricaoForm(request.POST)
 
         if form.is_valid():
-            aluno = form.cleaned_data['aluno']
-            curso = form.cleaned_data['curso']
+            aluno = form.cleaned_data["aluno"]
+            curso = form.cleaned_data["curso"]
 
-            # impede duplicidade
             if Inscricao.objects.filter(aluno=aluno, curso=curso).exists():
                 messages.error(request, "Este aluno já está inscrito neste curso.")
-                return redirect('inscricoes:criar')
+                return redirect("inscricoes:criar")
 
             form.save()
-            messages.success(request, "Inscrição cadastrada com sucesso!")
-            return redirect('inscricoes:listar')
-
+            messages.success(request, "Inscrição criada com sucesso!")
+            return redirect("inscricoes:listar")
     else:
         form = InscricaoForm()
 
-    return render(request, 'inscricoes/criar_inscricao.html', {'form': form})
+    return render(request, "inscricoes/criar.html", {"form": form})
+
+
+# EXCLUIR
+def excluir_inscricao(request, id):
+    inscricao = get_object_or_404(Inscricao, id=id)
+
+    if request.method == "POST":
+        inscricao.delete()
+        messages.success(request, "Inscrição removida com sucesso!")
+        return redirect("inscricoes:listar")
+
+    return render(request, "inscricoes/excluir.html", {"inscricao": inscricao})
