78b4582f890018ee1dd3f8e87a2e82f9786ef8c6
samaramerces1 <samaramerces746@gmail.com>
2025-11-26 21:33:55 -0300

    atualizações do models no app inscricoes

--- inscricoes/models.py
+++ inscricoes/models.py
@@ -1,3 +1,17 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from alunos.models import Aluno
+from cursos.models import Curso
+
+class Inscricao(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    data_inscricao = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('aluno', 'curso')  # evita inscrição duplicada
+
+    def __str__(self):
+        return f"{self.aluno.nome} → {self.curso.nome}"
+
+
